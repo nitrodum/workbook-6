38f18bc83feb81d9747ec36ce24e2a654cd971b0
Nima_Lama <nitrodum@yahoo.com>
2024-11-01 15:13:44 -0700

    added custom greeting

--- git-exercise/src/main/java/com/pluralsight/Main.java
+++ git-exercise/src/main/java/com/pluralsight/Main.java
@@ -7,5 +7,6 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter your name: ");
         String name = scanner.nextLine();
+        System.out.println("Hello " + name + "!");
     }
 }
