29dc1d7791e43654f7a4a8048ab9437373907cb6
Nima_Lama <nitrodum@yahoo.com>
2024-11-19 09:24:49 -0800

    Exercises

--- StreamsMiniExercises/src/main/java/com/pluralsight/Main.java
+++ StreamsMiniExercises/src/main/java/com/pluralsight/Main.java
@@ -1,7 +1,19 @@
 package com.pluralsight;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        try{
+            FileInputStream fis = new FileInputStream(".txt");
+            Scanner scanner = new Scanner(fis);
+        } catch (IOException e) {
+            System.out.println("Error");
+        }
+        Vehicle v = new Vehicle();
     }
 }
---
+++ StreamsMiniExercises/src/main/java/com/pluralsight/Program.java
@@ -0,0 +1,11 @@
+package com.pluralsight;
+
+import java.util.ArrayList;
+
+public class Program {
+    public static void main(String[] args) {
+        ArrayList<Integer> product = new ArrayList<Integer>();
+        product.add(1);
+        product.get(0);
+    }
+}
